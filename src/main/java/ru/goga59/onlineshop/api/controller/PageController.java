070a15d88c80101e538b196f27df13277f1897f5
goga-59 <egsad2004@gmail.com>
2025-02-26 15:24:36 +0500

    make PageController

--- src/main/java/ru/goga59/onlineshop/api/controller/PageController.java
+++ src/main/java/ru/goga59/onlineshop/api/controller/PageController.java
@@ -5,21 +5,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.goga59.onlineshop.api.service.NewsService;
+import ru.goga59.onlineshop.api.service.ProductService;
 import ru.goga59.onlineshop.storage.model.NewsModel;
+import ru.goga59.onlineshop.storage.model.ProductModel;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class NewsController {
+public class PageController {
 
     private final NewsService newsService;
+    private final ProductService tankCardService;
 
     @GetMapping("/")
     public String toNewsPage(Model model) {
         List<NewsModel> newsList = newsService.getAllNews();
         model.addAttribute("newsList", newsList);
         return "news";
+    }
+
+    @GetMapping("/info")
+    public String toInfoPage() {
+        return "info";
+    }
+
+    @GetMapping("/contacts")
+    public String toContactsPage() {
+        return "contacts";
+    }
+
+    @GetMapping("/catalog")
+    public String toCatalogPage(Model model) {
+        List<ProductModel> tankCards = tankCardService.getAllTanks();
+        model.addAttribute("productCards", tankCards);
+        return "catalog";
     }
 
 }
