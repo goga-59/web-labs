b79efd344a16d21b99202ac9bdb9a6cff1654bae
goga-59 <egsad2004@gmail.com>
2025-02-27 13:24:20 +0500

    start module inkt site

---
+++ site-iknt/src/main/java/ru/goga59/site_iknt/SiteIkntApplication.java
@@ -0,0 +1,13 @@
+package ru.goga59.site_iknt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteIkntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteIkntApplication.class, args);
+	}
+
+}
---
+++ site-iknt/src/test/java/ru/goga59/site_iknt/SiteIkntApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.goga59.site_iknt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteIkntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
